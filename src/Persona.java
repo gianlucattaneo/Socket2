4adadb74dfeeda7ac20b1c502b6105e4f82a6c45
Gianluca <Gianluca@192.168.1.33>
2021-03-16 22:44:54 +0100

    pedro

---
+++ src/Persona.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Persona {
+	private String nome;
+	private List<Persona> amici;
+	
+	Persona(String nome){
+		this.nome = nome;
+		this.amici = new ArrayList<Persona>();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public List<Persona> getAmici() {
+		return amici;
+	}
+	public void addAmico(Persona nome) {
+		this.amici.add(nome);
+	}
+	public boolean findAmico(String nome) {
+		boolean found = false;
+		for(Persona p : amici) {
+			if(p.nome.equals(nome)) {
+				found = true;
+				break;
+			}
+		}
+		return found;
+	}
+	public boolean deleteAmico(String nome) {
+		boolean removed = false;
+		for(Persona p : amici) {
+			if(p.nome.equals(nome)) {
+				amici.remove(p);
+				removed = true;
+				break;
+			}
+		}
+		return removed;
+	}
+	public String printAmici() {
+		String result = "";
+		for(Persona p : amici) {
+			result += p.getNome() + ", ";
+			}
+		return result.substring(0,result.length()-2);
+	}
+	public String toString() {
+		return String.format("Nome: %s", this.nome);
+	}
+}
